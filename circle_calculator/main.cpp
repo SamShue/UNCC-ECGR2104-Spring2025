cb40f5f7a5f399c0e2917038e812c7227358272e
Sam Shue <evilrobotfactory@gmail.com>
2025-01-16 15:39:01 -0500

    Functions Examples

---
+++ circle_calculator/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+const double PI = 3.14;
+
+double circleArea(double radius);
+double cylinderVolume(double radius, double length);
+double sphereVolume(double radius);
+
+int main(){
+    double r = 2.5;
+    double a = circleArea(r);
+    cout << "Area of the circle is " << a << endl;
+
+    double r2 = 3.3;
+    double l = 4.0;
+    double v = cylinderVolume(r2, l);
+    cout << "Volume of the cylinder is " << v << endl;
+
+    double r3 = 3.3;
+    double v2 = sphereVolume(r3);
+    cout << "Volume of the sphere is " << v2 << endl;
+
+    return 0;
+}
+
+double cylinderVolume(double radius, double length){
+    double volume = circleArea(radius) * length;
+    return volume;
+}
+
+double circleArea(double radius){
+    double area = PI * radius * radius;
+    return area;
+}
+
+double sphereVolume(double radius){
+    double volume = (4.0/3.0) * PI * radius * radius * radius;
+    return volume;
+}
---
+++ review/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // Example of boolean variable
+    bool b1 = true;
+    bool b2 = false;
+
+    // Example of character and string
+    char c = 'A';
+    string s = "Hello World";
+
+    // Example of doubles and ints and casting
+    int x = 5;
+    int y = 2;
+
+    double z = (double)x / (double)y;
+
+    cout << z << endl;
+
+    return 0;
+}
