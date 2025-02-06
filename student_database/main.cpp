c7a78cd1491fd0ccc6588ec9effcea13b9e0a9d7
Sam Shue <evilrobotfactory@gmail.com>
2025-02-06 15:37:51 -0500

    Construtors

--- student_database/main.cpp
+++ student_database/main.cpp
@@ -8,31 +8,58 @@ class Student {
     string name;
     int id;
 
-    void computeGpa(double grades[], int num_grades){
-        double avg = 0.0;
-        for(int i = 0; i < num_grades; i++){
-            avg = avg + grades[i];
-        }
-        avg = avg / num_grades;
-        gpa = avg;
-    }
+    Student();
+    Student(string name, double gpa) ;
 
-    void setGpa(double g){
-        if(g <= 4.0 && g >= 0.0){
-            gpa = g;
-        } else {
-            cout << "Invalid GPA!" << endl;
-            gpa = -1.0;
-        }
-    }
+    void computeGpa(double grades[], int num_grades);
 
-    double getGpa() const {
-        return gpa;
-    }
+    void setGpa(double gpa);
+    double getGpa() const;
 
     private:
     double gpa;
+    static int last_id;
 };
+
+int Student::last_id = 800000000;
+
+Student::Student(){
+    cout << "Constructor called!" << endl;
+    name = " ";
+    gpa = 4.0;
+    id = last_id + 1;
+    last_id = id;
+}
+
+Student::Student(string name, double gpa) {
+    cout << "Overloaded constructor called!" << endl;
+    this->name = name;
+    this->id = last_id + 1;
+    last_id = this->id;
+    setGpa(gpa);
+}
+
+void Student::computeGpa(double grades[], int num_grades){
+    double avg = 0.0;
+    for(int i = 0; i < num_grades; i++){
+        avg = avg + grades[i];
+    }
+    avg = avg / num_grades;
+    gpa = avg;
+}
+
+void Student::setGpa(double gpa){
+    if(gpa <= 4.0 && gpa >= 0.0){
+        this->gpa = gpa;
+    } else {
+        cout << "Invalid GPA!" << endl;
+        this->gpa = -1.0;
+    }
+}
+
+double Student::getGpa() const {
+    return gpa;
+}
 
 int main(){
 
@@ -40,12 +67,11 @@ int main(){
     Student s;
     s.name = "Sam";
     s.computeGpa(grades_s, 3);
-    s.id = 801234456;
 
-    Student s2;
-    s2.name = "Bob";
-    s2.setGpa(8.0);
-    s2.id = 801231285;
+    Student s2("Bob", 8.0);
+    //s2.name = "Bob";
+    //s2.setGpa(8.0);
+    //s2.id = 801231285;
 
     cout << "Name: " << s.name << endl;
     cout << "ID: " << s.id << endl;
