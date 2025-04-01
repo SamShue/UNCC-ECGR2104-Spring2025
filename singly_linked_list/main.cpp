512cf6f41dfcf934b77696bb37cffc9c2b0730b7
Sam Shue <evilrobotfactory@gmail.com>
2025-04-01 15:55:15 -0400

    Added size, at, insert, and remove

--- singly_linked_list/main.cpp
+++ singly_linked_list/main.cpp
@@ -26,12 +26,80 @@ void push_back(Node*& head, int data){
     temp->next = new Node(data);
 }
 
+void pop_back(Node*& head){
+    if(head == nullptr){
+        return;
+    }
+
+    if(head->next == nullptr){
+        delete head;
+        head = nullptr;
+        return;
+    }
+
+    Node* temp = head;
+
+    while(temp->next->next != nullptr){
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = nullptr;
+}
+
+int& at(Node* head, int index){
+    Node* temp = head;
+    for(int i = 0; i < index; i++){
+        temp = temp->next;
+    }
+    return temp->data;
+}
+
+int size(Node* head){
+    Node* temp = head;
+    int count = 0;
+    while(temp != nullptr){
+        temp = temp->next;
+        count++;
+    }
+    return count;
+}
+
+void insert(Node* head, int data, int index){
+    Node* temp = head;
+    for(int i = 0; i < (index - 1); i++){
+        temp = temp->next;
+    }
+
+    Node* backup = temp->next;
+    temp->next = new Node(data);
+    temp->next->next = backup;
+}
+
+void remove(Node* head, int index){
+    Node* temp = head;
+    for(int i = 0; i < (index - 1); i++){
+        temp = temp->next;
+    }
+    Node* backup = temp->next->next;
+    delete temp->next;
+    temp->next = backup;
+}
+
 int main(){
     Node* head = nullptr;
     
     push_back(head, 4);
     push_back(head, 7);
     push_back(head, 3);
+    push_back(head, 9);
+
+    insert(head, 1, 2);
+
+    remove(head, 3);
+
+    for(int i = 0; i < size(head); i++){
+        cout << at(head, i) << endl;
+    }
 
     return 0;
 }
